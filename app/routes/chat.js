78c6b4feac6217af0aab75d40e4a55639ba6b3ac
Fabricio Santos Guimarães <fabricioashua@gmail.com>
2017-08-15 13:44:12 -0300

    Update chat.js

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,9 @@
 module.exports = function (applicantion) {
     applicantion.post('/chat', function (req, res) {
-        applicantion.app.controllers.chat.init(applicantion, req, res);
+        applicantion.controllers.chat.init(applicantion, req, res);
     });
 
     applicantion.get('/chat', function (req, res) {
-        applicantion.app.controllers.chat.chat(applicantion, req, res);
+        applicantion.controllers.chat.chat(applicantion, req, res);
     });
 };
